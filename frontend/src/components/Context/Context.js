9c3b610b70f02c64a257178eccc2bd64437d444c
Victor Canal <victorhugocanal@gmail.com>
2025-12-12 03:14:43 -0500

    Context Update

--- frontend/src/components/Context/Context.js
+++ frontend/src/components/Context/Context.js
@@ -13,7 +13,7 @@ export function GlobalProvider({ children }) {
     //Servidor para strapi en BTS desarrollo AS
     //"http://10.147.18.240:1337"
     //Servidor para strapi en BTS desarrollo Azure
-    "http://btwweb-strapidev-b7aggpd6grckgjh0.eastus2-01.azurewebsites.net:1337"
+    "https://btsweb-strapi-development.azurewebsites.net/:1337"
   );
 
   // 🌍 Idioma global (persistente)
